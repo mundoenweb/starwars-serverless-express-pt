f62bdb59048123e55d279eb04533573df1b98c16
rommer <rommer@tuemprende.com>
2022-11-11 08:10:20 -0500

    updates

--- build/utils/helperResponse.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helperResponse = void 0;
-const helperResponse = (res, data, code = 200, message) => {
-    const response = { data, message, status: code };
-    res.status(code);
-    res.json(response);
-};
-exports.helperResponse = helperResponse;
